967e31fd5a5ece6580c1a75cdbeb2f5232444f75
svdvoort <23049683+Svdvoort@users.noreply.github.com>
2024-07-16 11:24:45 +0200

    Autoget author name and project

--- project_name/.project_init.sh
+++ project_name/.project_init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is necessary to execute some tasks
+# only on first start-up
 
 if [[ $PWD == /tmp* ]]; then
     echo "This is copier update. Exiting"
@@ -10,21 +12,33 @@ if [ -d .git/ ]; then
   exit 0
 fi
 
+# Generate license
 lice $1 -o $2 -p $3 > $3/LICENSE
 
+# Set-up pyenv
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
+
+# Use pyenv to install and setup
+# requested python version
 pyenv install -s $5
 pyenv local $5
 pyenv shell $5
-pyenv exec pip install poetry
-poetry env use $5
-poetry install --with dev,docs,cruft
 
+# Install python poetry in local python version
+pyenv exec pip install poetry
+
+# Set-up virtual environment using poetry
+poetry env use $5
+poetry run pip install --upgrade pip
+poetry install --with dev,docs,copier
+
+# Initial setup of git
 git init --initial-branch="main"
 git add .
 git commit -m "Initial commit"
 git remote add origin $4
 git push --set-upstream origin main
 
+# Set-up precommit
 poetry run pre-commit install
